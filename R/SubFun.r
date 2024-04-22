1d1c1382387f81ddc22057161c373c4da04c77be
LienAlbert <tmpss95395@gmail.com>
2024-04-22 16:53:07 +0800

    level default

--- R/SubFun.r
+++ R/SubFun.r
@@ -1033,6 +1033,12 @@ iNEXTbeta.PDlink <- function(data, level, datatype='abundance', q = c(0, 1, 2),
   
   
   max_alpha_coverage = F
+  
+  if(is.null(level)){
+    level = seq(0.5,1, 0.025)
+  }else{
+    levle = level
+  }
 
   if(datatype=='abundance'){
 
